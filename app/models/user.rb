c01790189bb848c159c7c04f8764c67ea1b7c123
Rui-Tanizaki <155947121+Rui-Tanizaki@users.noreply.github.com>
2024-02-01 21:08:10 +0900

    ユーザー管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
          
          validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "is invalid. Include both letters and numbers" }
          validates :nickname, presence: true
-         validates :name_last_kanji, presence: true, format: { with: /\A[^ぁ-んァ-ヶー－a-zA-Z0-9]+\z/, message: 'is invalid. Input full-width characters' }
-         validates :name_first_kanji, presence: true, format: { with: /\A[^ぁ-んァ-ヶー－a-zA-Z0-9]+\z/, message: 'is invalid. Input full-width characters' }
+         validates :name_last_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠]+\z/, message: 'is invalid. Input full-width characters' }
+         validates :name_first_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠]+\z/, message: 'is invalid. Input full-width characters' }
          validates :name_last_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
          validates :name_first_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
          validates :birth_date, presence: true
