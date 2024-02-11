5f9cd2cd8e6c7c98c3be2b751fabbbcd39b3f324
Rui-Tanizaki <155947121+Rui-Tanizaki@users.noreply.github.com>
2024-02-11 09:41:12 +0900

    訂正

--- app/models/Item_order.rb
+++ app/models/Item_order.rb
@@ -3,7 +3,8 @@ class ItemOrder
   attr_accessor :token,:order_postcode, :prefecture_id, :order_city, :order_address, :order_phone_number, :order_building, :user_id, :item_id
 
   with_options presence: true do
-
+    validates :user_id
+    validates :item_id
     validates :token
     validates :order_postcode, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
